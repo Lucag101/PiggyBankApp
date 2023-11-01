a2d2eb0d5fb79ae96981463589b8fa542e22582b
Lucas Gandola <lucasbg@byu.edu>
2023-11-01 14:50:20 -0600

    added parent Modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,21 @@
 $(document).ready(function() {
+    
     let totalMoney = 0;
 
         // Sample data
     let earningsData = ["Bought Jeans -30", "Watered the plants +5", "Walked the dog +5", "Did the dishes  +3"];
     let choresData =[
-        {'chore': 'Sweep the garage', 'points': 3},
-        {'chore': 'Make your bed every day', 'points': 3},
-        {'chore': 'Walk the dog', 'points': 4},
-        {'chore': 'Do the dishes', 'points': 2},
-        {'chore': 'Take out the trash', 'points': 1},
+        {'chore': 'Sweep the garage', 'points': 3, 'continual': true},
+        {'chore': 'Make your bed every day', 'points': 3 , 'continual': false},
+        {'chore': 'Walk the dog', 'points': 4, 'continual': false},
+        {'chore': 'Do the dishes', 'points': 2, 'continual': true},
+        {'chore': 'Take out the trash', 'points': 1, 'continual': false},
+        {'chore': 'Clean your room', 'points': 5, 'continual': true},
+        {'chore': 'Do your homework', 'points': 5, 'continual': true},
+        {'chore': 'Wash the car', 'points': 10, 'continual': false}
     ]
 
-    function populateData(){
-         // Populate earnings
-        for(let item of earningsData) {
-            $('#earningsList').append(`<li>${item}</li>`);
-        }
-
-        // Populate chores
-        for(let chore of choresData) {        
-            
-            addLine =  $('<li>').html(chore['chore'] + "<span style='color:green'> +" + chore['points'] + "</span>").click(function() {
-                totalMoney += chore['points'];
-                $(this).remove();
-                updateDisplay()
-            }); 
-
-            $('#choresList').append(addLine);
-        }
-        // updateDisplay()
-    };
-   
+//_____________________EVENT LISTENERS___________________________
 
     // Add event listeners for the buttons
     $('#addMoneyButton').click(function() {
@@ -43,20 +28,44 @@ $(document).ready(function() {
         updateDisplay()
     });
 
+    $('#choreAdder').click(function() {
+        $("#parentToolModal").removeClass('invisible')
+    });
 
 
-    // $("#earningsList").on('click', function() {
-    //     const moneyToAdd = 5; // You can change this or make it dynamic
-    //     totalMoney += moneyToAdd;
-    //     updateDisplay();
-    // });
 
-    // $("#choresList").on('click', function() {
-    //     const moneyToSubtract = 5; // You can change this or make it dynamic
-    //     totalMoney -= moneyToSubtract;
-    //     updateDisplay();
-    // });
 
+//_____________________FUNCTIONS___________________________
+
+    function populateData(){
+         // Populate earnings
+        for(let item of earningsData) {
+            $('#earningsList').append(`<li>${item}</li>`);
+        }
+
+        // Populate chores
+        for(let chore of choresData) {        
+            addLine =  $('<li>').html(chore['chore'] + "<span style='color:green'> +" + chore['points'] + "</span>")
+            if (chore['continual'] == false){
+               addLine.click(function() {
+                    totalMoney += chore['points'];
+                    $(this).remove();
+                    updateDisplay()
+                }); 
+            } else {
+                //append onto the html a new span
+                addLine.prepend("<span style='color:blue'> (Weekly) </span>")
+                    .click(function() {
+                        totalMoney += chore['points'];
+                        updateDisplay()
+                    });
+            }
+
+            $('#choresList').append(addLine);
+        }
+        // updateDisplay()
+    };
+   
     function substractDollar() {
         totalMoney -= 1;
     }
@@ -74,6 +83,7 @@ $(document).ready(function() {
         });
     }
 
+    
     updateDisplay()
     populateData()
 });
